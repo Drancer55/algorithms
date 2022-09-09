5e36a1e6fbff275dade5c53e0c36ec57aafdb77e
Drancer55 <antonio281255@gmail.com>
2022-09-09 00:47:38 -0500

    start of the math operations form

--- algorithms/src/App.js
+++ algorithms/src/App.js
@@ -6,17 +6,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <fieldset>
+            <div className='operation'>
+              <label htmlFor='fibonacci'>Fibonacci: </label>
+                <input type={'number'} id="fibonacci"/>
+            </div>
+            <h5> - </h5>
+            <div className='operation'>
+              <label htmlFor='triangular'>2Triangular: </label>
+                <input type={'number'} id="triangular"/>
+            </div>
+            <h5> + </h5>
+            <div className='operation'>
+              <label htmlFor='primo'>Primo: </label>
+                <input type={'number'} id="primo"/>
+            </div>
+          </fieldset>
+          <h5> = </h5>
+          <fieldset>
+            <div className='operation'>
+              <label htmlFor='result'>Resultado: </label>
+              <input type={'number'} id="result" />
+            </div>
+          </fieldset>
+        </form>
       </header>
     </div>
   );
